a91ce3477a1d24abb04788576ee65a555395fc97
Elizabeht Murray <lizmurray479@gmail.com>
2017-01-09 21:57:29 -0500

    Fixed issue regarding file being printed out incorrectly. Now passes (revised) Sortable validation.

--- sort.py
+++ sort.py
@@ -1,27 +1,31 @@
-import re  # Using re to do a search through the listings.
+from collections import defaultdict
 import json  # Import JSON to handle read/write of json objects.
-prod = 0
-data = {}  # Global declaration to prevent it being recreated
 with open("data/products.txt", "r") as complete_products:
+    prod = 0  # Declaring global counter
     for products in complete_products:
-        prod = prod + 1  # Used as a counter to show progress
-        print("Product #" + str(prod))
+        # Global declaration to prevent it being recreated
+        data = defaultdict(list)
+        #  list_complete initialized and set to null.
+        list_complete = []
+        # Used as a counter to show progress
+        prod = prod + 1
+        print("Product #" + str(prod))  # Printing to track progress
         product = json.loads(products)  # Loading JSON to Dict
         # Normalizing the input for searching.
-        product["product_name"] = product["product_name"].replace("_", " ")
+        search_product = product["product_name"].replace("_", " ")
         # Opening the listings
         with open("data/listings.txt", "r") as complete_listings:
             for listings in complete_listings:
                 listing = json.loads(listings)
-                with open("data/output.json", "a") as output:
-                    if product["product_name"] in str(listing):
-                        listing["product"] = product["product_name"]
-                        json.dump(listing, output)
-                        # print(data)
-                        # output.write(json.dumps + "\n")
-                        # json.dump(listing, output)
+                if search_product in listing["title"]:
+                    list_complete.append(listing)
 
-                # data2 = {"people": [{"n": "d", "w": "as", "f": "nb"}]}
-                # with open("data/output.json", "a") as output:
-                #     json.dump(data, output)
-                #     data = None
+        # Called at the end of each product. Only if entry exists.
+        if len(list_complete) >= 1:
+            # Write to the text file
+            with open("data/output.txt", "a") as output:
+                data["product_name"] = product["product_name"]
+                # Writing complete list to file to avoid unhashable errors.
+                data["listings"] = list_complete
+                json.dump(data, output)
+                output.write(" " + "\n")  # Writing new line.
